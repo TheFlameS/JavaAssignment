57746427b74554b1e93e37e6133776b56dd5960f
TheFlameS <york_trees@hotmail.com>
2018-05-11 23:31:57 +0700

    3rd commit of gui

--- src/com/company/gui/MainGui.java
+++ src/com/company/gui/MainGui.java
@@ -2,20 +2,90 @@ package com.company.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainGui extends JFrame{
     public MainGui (){
         super("Pokemon");
 
         Container c = getContentPane();
-        c.setLayout(new GridLayout(2,2));
+        c.setLayout(new BorderLayout());
 
-        JPanel jPanel = new JPanel();
-        jPanel.setLayout();
+        //Top Menu Section
+        JPanel jPanelTopMenu = new JPanel();
+        jPanelTopMenu.setLayout(new FlowLayout());
 
-        JLabel jLabel = new JLabel("Something");
+        JRadioButton farm = new JRadioButton("Farm");
+        JRadioButton forest = new JRadioButton("Forest");
 
-        c.add(jLabel);
+        jPanelTopMenu.add(farm);
+        jPanelTopMenu.add(forest);
+
+        farm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Blah");
+            }
+        });
+
+        forest.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Blah");
+            }
+        });
+
+        c.add(jPanelTopMenu, BorderLayout.PAGE_START);
+
+        //Bottom Btn Section
+        JPanel jPanelBottomBtn = new JPanel();
+        jPanelBottomBtn.setLayout(new FlowLayout());
+
+        JButton list = new JButton("List");
+        JButton add = new JButton("Add");
+        JButton feed = new JButton("Feed");
+
+        jPanelBottomBtn.add(list);
+        jPanelBottomBtn.add(add);
+        jPanelBottomBtn.add(feed);
+
+        list.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Blah");
+            }
+        });
+
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Blah");
+            }
+        });
+
+        feed.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Blah");
+            }
+        });
+
+        c.add(jPanelBottomBtn, BorderLayout.PAGE_END);
+
+        //center
+        JPanel center = new JPanel();
+        center.setLayout(new GridLayout(0,2));
+
+        JLabel pokemonIcon = new JLabel("");
+        JLabel pokemonStatus = new JLabel("There is nothing...");
+        Icon img = new ImageIcon(getClass().getResource("./image_files/Nothing.png"));
+        pokemonIcon.setIcon(img);
+
+        center.add(pokemonIcon);
+        center.add(pokemonStatus);
+
+        c.add(center, BorderLayout.CENTER);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
